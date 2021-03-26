381c7df129c8e24998ea4f713a1d140c99fec560
BunyaminKucuk <BunyaminKucuk@github.com>
2021-03-26 09:58:21 +0300

    first commit

---
+++ src/pages/Taskview/Taskview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Taskview',
+  templateUrl: './Taskview.component.html',
+  styleUrls: ['./Taskview.component.scss']
+})
+export class TaskviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
